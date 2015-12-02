240ebff2f65ad4030280260881a1e9bc81576559
Satish Verma <satish.verma143@gmail.com>
2015-12-02 14:59:39 +0530

    add commit

--- www/js/app.js
+++ www/js/app.js
@@ -9,6 +9,7 @@ app.run(function ($ionicPlatform) {
     $ionicPlatform.ready(function () {
         // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
         // for form inputs)
+
         if (window.cordova && window.cordova.plugins.Keyboard) {
             cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
         }
